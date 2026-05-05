a934c433ac476b1cb89e086eb8ec56802e77dd00
450186 <146966554+450186@users.noreply.github.com>
2026-05-05 16:17:35 +0100

    vegetarian

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -622,7 +622,7 @@ app.get("/debug/find-potw-candidates", async (_req, res) => {
   try {
     const searchUrl =
       "https://world.openfoodfacts.org/cgi/search.pl" +
-      "?search_terms=whole" +
+      "?search_terms=vegetarian" +
       "&search_simple=1" +
       "&action=process" +
       "&json=1" +
