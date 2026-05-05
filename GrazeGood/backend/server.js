32ccd121df2f3806aa600b519d2e817d27e0c38e
450186 <146966554+450186@users.noreply.github.com>
2026-05-05 14:19:56 +0100

    attempt to get more POTWs

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -623,7 +623,7 @@ app.get("/debug/find-potw-candidates", async (_req, res) => {
   try {
     const searchUrl =
       "https://world.openfoodfacts.org/cgi/search.pl" +
-      "?search_terms=organic" +
+      "?search_terms=oats" +
       "&search_simple=1" +
       "&action=process" +
       "&json=1" +
