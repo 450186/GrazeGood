9d2b150de8c82e7edfc32f793b690293be5e2762
450186 <146966554+450186@users.noreply.github.com>
2026-05-05 14:54:31 +0100

    forgot to save backend

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -590,18 +590,14 @@ app.get("/products-of-the-week", async (_req, res) => {
       POTWs.map((barcode) => getProduct(barcode))
     );
 
-    // const goodProducts = products
-    //   .filter(Boolean)
-    //   .filter((product) =>
-    //     product.eco?.ecoScore != null &&
-    //     product.eco.ecoScore >= 70 &&
-    //     !product.eco.ecoReason?.some((reason) => reason.impact === "high")
-    //   );
     const goodProducts = products
-  .filter(Boolean)
-  .filter((product) =>
-    product.eco?.ecoScore != null
-  );
+      .filter(Boolean)
+      .filter((product) =>
+        product.eco?.ecoScore != null &&
+        product.eco.ecoScore >= 70 &&
+        !product.eco.ecoReason?.some((reason) => reason.impact === "high")
+      );
+
 
     const week = setWeekNum();
 
