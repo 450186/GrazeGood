c80cb9f36aa33f0d6238f820c662235dc3f78baa
450186 <146966554+450186@users.noreply.github.com>
2026-05-17 16:34:06 +0100

    Fair trade

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -622,7 +622,7 @@ app.get("/debug/find-potw-candidates", async (_req, res) => {
   try {
     const searchUrl =
       "https://world.openfoodfacts.org/cgi/search.pl" +
-      "?search_terms=organic" +
+      "?search_terms=fair trade" +
       "&search_simple=1" +
       "&action=process" +
       "&json=1" +
