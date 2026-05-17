290a11dacdf1845f2f82dd6f2f890b9da3f221e7
450186 <146966554+450186@users.noreply.github.com>
2026-05-17 17:08:47 +0100

    dark chocolate

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -622,7 +622,7 @@ app.get("/debug/find-potw-candidates", async (_req, res) => {
   try {
     const searchUrl =
       "https://world.openfoodfacts.org/cgi/search.pl" +
-      "?search_terms=eco friendly" +
+      "?search_terms=dark chocolate" +
       "&search_simple=1" +
       "&action=process" +
       "&json=1" +
