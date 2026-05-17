6c21135e649d2f9c3494dfefd2e06f7c3d34552f
450186 <146966554+450186@users.noreply.github.com>
2026-05-17 17:17:49 +0100

    rice

--- GrazeGood/backend/server.js
+++ GrazeGood/backend/server.js
@@ -622,7 +622,7 @@ app.get("/debug/find-potw-candidates", async (_req, res) => {
   try {
     const searchUrl =
       "https://world.openfoodfacts.org/cgi/search.pl" +
-      "?search_terms=wholegrain" +
+      "?search_terms=rice" +
       "&search_simple=1" +
       "&action=process" +
       "&json=1" +
